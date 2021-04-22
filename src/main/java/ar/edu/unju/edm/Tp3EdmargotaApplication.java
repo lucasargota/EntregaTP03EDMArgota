1cabd46307382b5347976a7f6eddecdc756861c6
Lucas <Lucas@DESKTOP-FVGHMLF>
2021-04-22 12:47:32 -0300

    Lucas Emmanuel Argota Inicializacion del proyecto Implementacion de  @qualifier

---
+++ src/main/java/ar/edu/unju/edm/Tp3EdmargotaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3EdmargotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3EdmargotaApplication.class, args);
+	}
+
+}
